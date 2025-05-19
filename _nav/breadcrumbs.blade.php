1418a4efec729051023530bb74ba34714aa14240
Burundug <iburundugi@gmail.com>
2025-05-19 16:23:47 +0300

    Убран метод, который НЕ должен там находится #2

--- _nav/breadcrumbs.blade.php
+++ _nav/breadcrumbs.blade.php
@@ -5,26 +5,26 @@
 @endphp
 
 <section>
-    @php 
+    @php
     echo "<pre>";
-         $breadcrumbs_array = generateBreadcrumbs($page);
+         $breadcrumbs_array = $page->generateBreadcrumbs();
          //print_r($breadcrumbs_array);
     echo "</pre>";
     @endphp
     <ul class="sf-breadcrumb">
           <li class="sf-breadcrumb-item">
             <a href="#" class="text-1/2">
-              <i class="color-primary sf-icon sf-icon-light sf-breadcrumb--primary-color">home</i>  
+              <i class="color-primary sf-icon sf-icon-light sf-breadcrumb--primary-color">home</i>
             </a>
           </li>
-          
+
     @foreach($breadcrumbs_array as $key => $item)
         <li class="sf-breadcrumb-item text-1/2 sf-breadcrumb--primary-color">
           <a href="#" class="text-1/2">{{$item['title']}}</a>
             <i class="sf-icon sf-icon-light sf-breadcrumb--primary-color">chevron_right</i>
           </li>
     @endforeach
-    
+
           <!--<li class="sf-breadcrumb-item">
             <a href="#" class="text-1/2">Главная</a>
             <i class="sf-icon sf-icon-light">chevron_right</i>
