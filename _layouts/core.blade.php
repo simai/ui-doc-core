69be36da05c7ca9977964c881dce80e2532d2fa9
Burundug <iburundugi@gmail.com>
2025-05-19 17:20:13 +0300

    Получаем токен коммита для обхода кеша github #2

--- _layouts/core.blade.php
+++ _layouts/core.blade.php
@@ -2,4 +2,4 @@
     window.sfPath = `https://cdn.jsdelivr.net/gh/simai/ui@${@json($page->sha ?? 'latest')}/distr/`;
 </script>
 <script src="{{'https://cdn.jsdelivr.net/gh/simai/ui@' . $page->sha . '/distr/core/js/core.js'}}"></script>
-<link rel="stylesheet" href={{'https://cdn.jsdelivr.net/gh/simai/ui@' .  $page->sha . '/distr/core/css/core.css'}}/>
+<link rel="stylesheet" href="{{'https://cdn.jsdelivr.net/gh/simai/ui@' .  $page->sha . '/distr/core/css/core.css'}}"/>
