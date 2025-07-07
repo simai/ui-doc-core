22ce529e405551e1225e45a48fff979ff8636161
Burundug <iburundugi@gmail.com>
2025-07-07 10:35:04 +0300

    Iteration 2

--- config.php
+++ config.php
@@ -1,67 +1,81 @@
 <?php
 
-use Illuminate\Support\Str;
+    use Illuminate\Support\Str;
 
-return [
-    'baseUrl' => '',
-    'production' => false,
-    'siteName' => 'Simai Documentation',
-    'siteDescription' => 'Simai framework documentation',
+    return [
+        'baseUrl' => '',
+        'production' => false,
+        'siteName' => 'Simai Documentation',
+        'siteDescription' => 'Simai framework documentation',
+        'github' => 'https://github.com/simai/ui-doc-template/',
 
-    'docsearchApiKey' => env('DOCSEARCH_KEY'),
-    'docsearchIndexName' => env('DOCSEARCH_INDEX'),
-    'locales' => [
-        'en' => 'English',
-        'ru' => 'Русский',
-        'de' => 'Deutsch',
-        'es' => 'Spanish',
-    ],
-    'defaultLocale' => 'ru',
-    'lang_path' => 'source/lang',
-    'getNavItems' => function ($page) {
-        return $page->configurator->getPrevAndNext($page->getPath(), $page->locale());
-    },
-    'generateBreadcrumbs' => function ($page) {
-        $currentPath = trim($page->getPath(), '/');
-        $locale = $page->locale();
-        $segments = $currentPath === '' ? [] : explode('/', $currentPath);
-        return $page->configurator->generateBreadCrumbs($locale, $segments);
-    },
-    'locale' => function ($page) {
-        $path = str_replace('\\', '/', $page->getPath());
-        $locale = explode('/', $path);
-        $current = 'ru';
-        $locales = array_keys($page->locales->toArray());
-        foreach ($locale as $segment) {
-            if (in_array($segment, $locales)) {
-                $current = $segment;
-                break;
+        'docsearchApiKey' => env('DOCSEARCH_KEY'),
+        'docsearchIndexName' => env('DOCSEARCH_INDEX'),
+        'locales' => [
+            'en' => 'English',
+            'ru' => 'Русский',
+            'de' => 'Deutsch',
+            'es' => 'Spanish',
+        ],
+        'defaultLocale' => 'ru',
+        'lang_path' => 'source/lang',
+        'getNavItems' => function ($page) {
+            return $page->configurator->getPrevAndNext($page->getPath(), $page->locale());
+        },
+        'generateBreadcrumbs' => function ($page) {
+            $currentPath = trim($page->getPath(), '/');
+            $locale = $page->locale();
+            $segments = $currentPath === '' ? [] : explode('/', $currentPath);
+            return $page->configurator->generateBreadCrumbs($locale, $segments);
+        },
+        'locale' => function ($page) {
+            $path = str_replace('\\', '/', $page->getPath());
+            $locale = explode('/', $path);
+            $current = 'ru';
+            $locales = array_keys($page->locales->toArray());
+            foreach ($locale as $segment) {
+                if (in_array($segment, $locales)) {
+                    $current = $segment;
+                    break;
+                }
             }
-        }
-        return $current;
-    },
-    'isHome' => function ($page) {
-        $current = trim($page->getPath(), '/');
-        return $current === $page->locale();
-    },
-    'collections' => require_once('source/_core/collections.php'),
-    'isActive' => function ($page, $path) {
+            return $current;
+        },
+        'gitHubUrl' => function ($page) {
+            $path = str_replace('\\', '/', $page->getPath());
+            $lang = $page->locale();
+            $arPath = explode('/', $path);
+            $arShift = array_slice($arPath, 2);
 
-        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
-    },
-    'translate' => function ($page, $text) {
-        return $page->configurator->getTranslate(trim($text), $page->locale());
-    },
-    'isActiveParent' => function ($page, $node): bool {
-        $currentPath = $page->getPath();
-        if ($node['path'] === $currentPath) {
-            return true;
-        }
-        foreach ($node['children'] as $child) {
-            if ($page->isActiveParent($child, $currentPath)) {
+            if(count($arShift) > 0) {
+                $path = "_docs-{$lang}" . '/' . implode('/', $arShift) . '.md';
+            } else {
+                $path = "_docs-{$lang}/index.md";
+            }
+            return $path;
+        },
+        'isHome' => function ($page) {
+            $current = trim($page->getPath(), '/');
+            return $current === $page->locale();
+        },
+        'collections' => require_once('source/_core/collections.php'),
+        'isActive' => function ($page, $path) {
+
+            return Str::endsWith(trimPath($page->getPath()), trimPath($path));
+        },
+        'translate' => function ($page, $text) {
+            return $page->configurator->getTranslate(trim($text), $page->locale());
+        },
+        'isActiveParent' => function ($page, $node): bool {
+            $currentPath = $page->getPath();
+            if ($node['path'] === $currentPath) {
                 return true;
             }
-        }
-        return false;
-    },
-];
+            foreach ($node['children'] as $child) {
+                if ($page->isActiveParent($child, $currentPath)) {
+                    return true;
+                }
+            }
+            return false;
+        },
+    ];
