97d9ec750ddf1ee0b28a379c5afef7780616186f
Burundug <iburundugi@gmail.com>
2025-10-23 11:06:21 +0300

    update docs

--- collections.php
+++ collections.php
@@ -1,20 +1,20 @@
 <?php
 
+
     use Illuminate\Support\Str;
-
     $collections = [];
-    foreach (glob('./source/_docs-*', GLOB_ONLYDIR) as $dir) {
-        $lang = str_replace('_docs-', '', basename($dir));
+    $collectionName = collect(explode('/', trim(str_replace('\\', '/', $_ENV["DOCS_DIR"]), '/')))->implode('-');
+    foreach (glob( './source/'. $_ENV['DOCS_DIR'] . '/*', GLOB_ONLYDIR) as $dir) {
+        $lang = basename($dir);
+        $collections["{$collectionName}-{$lang}"] = [
+            'directory' => basename( '/source/'. $_ENV['DOCS_DIR']),
 
-        $collections["docs-$lang"] = [
-            'directory' => basename($dir),
             'language' => $lang,
             'extends' => '_core._layouts.documentation',
             'filter' => fn($page) => $page->_meta->extension === 'md',
             'path' => function ($page) use ($lang) {
                 $relative = str_replace('\\', '/', $page->_meta->relativePath);
                 $rest = trim(Str::after($relative, "$lang/"), '/');
-
                 return "$lang/" . trim(
                         rtrim($rest, '/') . (
                         basename($rest) !== $page->_meta->filename
@@ -26,5 +26,4 @@
             },
         ];
     }
-
     return $collections;
