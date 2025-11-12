8b0694326dcef9b5f193b8e11563ec3be8fba9b9
Burundug <iburundugi@gmail.com>
2025-11-12 10:11:12 +0300

    change distr path

--- _layouts/core.blade.php
+++ _layouts/core.blade.php
@@ -1,9 +1,10 @@
 @php
 $hasSha = $page->sha ?? 'latest';
+$distPath = "https://cdn.jsdelivr.net/gh/simai/ui@{$hasSha}/distr/"
 @endphp
 <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
 <script>
-    window.sfPath = `https://cdn.jsdelivr.net/gh/simai/ui@${@json($hasSha)}/distr/`;
+    window.sfPath = "{{$distPath}}";
 </script>
 <script src="{{'https://cdn.jsdelivr.net/gh/simai/ui@'. $hasSha . '/distr/core/js/core.js'}}"></script>
 <link rel="stylesheet" href="{{'https://cdn.jsdelivr.net/gh/simai/ui@'. $hasSha . '/distr/core/css/core.css'}}"/>
